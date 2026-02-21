4a0f0af41d4311908157e491bbad2eae658ed54a
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-21 17:29:56 -0500

    build: remove godot registry plugin
    Still failing

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("buildlogic.java-library-conventions")
     id("buildlogic.java-styles-conventions")
     id("buildlogic.java-null-check")
-    id("buildlogic.jvm-godot-registry")
     id("buildlogic.godot-export-conventions")
 }
 
