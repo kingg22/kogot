ecc580ba23b798caba242d4bf69fdcffa602ce83
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-22 15:44:30 -0500

    build: add kotlin language to bridge module

--- godot-java-bridge/build.gradle.kts
+++ godot-java-bridge/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("buildlogic.java-library-conventions")
     id("buildlogic.java-styles-conventions")
     id("buildlogic.java-null-check")
+    id("buildlogic.kotlin-library-conventions")
+    id("buildlogic.kotlin-styles-conventions")
 }
 
 dependencies {
