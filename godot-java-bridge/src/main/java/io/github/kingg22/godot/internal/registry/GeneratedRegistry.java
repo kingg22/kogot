d73a16e1e8afbe04bd87cb2d9de3cc061c485c71
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-22 10:01:50 -0500

    test(bridge): add JvmTestNode for testing and remove unused code

--- godot-java-bridge/src/main/java/io/github/kingg22/godot/internal/registry/GeneratedRegistry.java
+++ godot-java-bridge/src/main/java/io/github/kingg22/godot/internal/registry/GeneratedRegistry.java
@@ -1,5 +1,6 @@
 package io.github.kingg22.godot.internal.registry;
 
+import io.github.kingg22.godot.api.GodotClass;
 import io.github.kingg22.godot.api.GodotRegistry;
 
 /** Placeholder registry. Overwritten by build task. */
@@ -8,5 +9,13 @@ public final class GeneratedRegistry {
         throw new UnsupportedOperationException("Utility class");
     }
 
-    public static void registerAll(final GodotRegistry registry) {}
+    public static void registerAll(final GodotRegistry registry) {
+        registry.register("JvmTestNode", "Node", JvmTestNode::new);
+    }
+
+    public static final class JvmTestNode implements GodotClass {
+        public JvmTestNode() {
+            System.out.println("JvmTestNode created");
+        }
+    }
 }
