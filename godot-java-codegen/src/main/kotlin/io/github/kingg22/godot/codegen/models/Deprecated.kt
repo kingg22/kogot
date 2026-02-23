bb5a2689b93f5c56458d773428d97b79ec0286ed
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-23 18:59:26 -0500

    feat(codegen): add models for gdetension_interface json schema with polymorphic types

---
+++ godot-java-codegen/src/main/kotlin/io/github/kingg22/godot/codegen/models/Deprecated.kt
@@ -0,0 +1,11 @@
+package io.github.kingg22.godot.codegen.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Deprecated(
+    @SerialName("since") val since: String,
+    @SerialName("message") val message: String? = null,
+    @SerialName("replace_with") val replaceWith: String? = null,
+)
