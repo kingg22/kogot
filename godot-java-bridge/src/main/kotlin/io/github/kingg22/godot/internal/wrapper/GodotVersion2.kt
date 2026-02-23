1d1fb4fb2648144b4c31a82cf93a4c7480fc6d80
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-23 00:34:19 -0500

    feat(bridge): add comparable for GodotVersion2
    Copied from KotlinVersion

--- godot-java-bridge/src/main/kotlin/io/github/kingg22/godot/internal/wrapper/GodotVersion2.kt
+++ godot-java-bridge/src/main/kotlin/io/github/kingg22/godot/internal/wrapper/GodotVersion2.kt
@@ -3,6 +3,8 @@ package io.github.kingg22.godot.internal.wrapper
 import io.github.kingg22.godot.internal.ffm.GDExtensionGodotVersion2
 import java.lang.foreign.MemorySegment
 
+private const val MAX_COMPONENT_VALUE = 255
+
 /**
  * ```c++
  * struct {
@@ -39,8 +41,9 @@ data class GodotVersion2 private constructor(
     val hash: String,
     val timestamp: Long,
     val string: String,
-) {
+) : Comparable<GodotVersion2> {
     override fun toString(): String = string
+    private val version get() = versionOf(major, minor, patch)
 
     constructor(struct: MemorySegment) : this(
         major = GDExtensionGodotVersion2.major(struct),
@@ -53,4 +56,36 @@ data class GodotVersion2 private constructor(
         timestamp = GDExtensionGodotVersion2.timestamp(struct),
         string = GDExtensionGodotVersion2.string(struct).getString(0),
     )
+
+    /*
+     * Took from kotlin.KotlinVersion
+     *
+     * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+     * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+     */
+    override fun compareTo(other: GodotVersion2): Int = version - other.version
+
+    private fun versionOf(major: Int, minor: Int, patch: Int): Int {
+        require(
+            major in 0..MAX_COMPONENT_VALUE && minor in 0..MAX_COMPONENT_VALUE && patch in 0..MAX_COMPONENT_VALUE,
+        ) {
+            "Version components are out of range: $major.$minor.$patch"
+        }
+        return major.shl(16) + minor.shl(8) + patch
+    }
+
+    /**
+     * Returns `true` if this version is not less than the version specified
+     * with the provided [major] and [minor] components.
+     */
+    fun isAtLeast(major: Int, minor: Int): Boolean = // this.version >= versionOf(major, minor, 0)
+        this.major > major || (this.major == major && this.minor >= minor)
+
+    /**
+     * Returns `true` if this version is not less than the version specified
+     * with the provided [major], [minor] and [patch] components.
+     */
+    fun isAtLeast(major: Int, minor: Int, patch: Int): Boolean = // this.version >= versionOf(major, minor, patch)
+        this.major > major ||
+            (this.major == major && (this.minor > minor || (this.minor == minor && this.patch >= patch)))
 }
