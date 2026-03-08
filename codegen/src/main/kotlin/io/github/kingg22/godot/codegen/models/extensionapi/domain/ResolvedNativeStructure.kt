f72a85ec4704d6fa781c65ddb417a64fe8a6258a
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-08 11:43:48 -0500

    perf(codegen): parse Native Structure format in resolved domain class

--- codegen/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensionapi/domain/ResolvedNativeStructure.kt
+++ codegen/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensionapi/domain/ResolvedNativeStructure.kt
@@ -1,8 +1,10 @@
 package io.github.kingg22.godot.codegen.models.extensionapi.domain
 
+import io.github.kingg22.godot.codegen.impl.extensionapi.native.resolver.NativeStructureParser
 import io.github.kingg22.godot.codegen.models.extensionapi.NativeStructure
 
 data class ResolvedNativeStructure(val raw: NativeStructure) {
     val name: String get() = raw.name
     val shortName: String get() = raw.name.substringAfterLast('.')
+    val fields = NativeStructureParser.parseFormat(raw.format)
 }
