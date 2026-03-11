344a62c4815144bbee880b72dfe21fe2401d5367
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-10 21:07:16 -0500

    feat(native api): add utility for `kotlin.String` conversions and variants

---
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/builtin/Utils.kt
@@ -0,0 +1,13 @@
+package io.github.kingg22.godot.api.builtin
+
+public fun String.asGodotString(): GodotString = GodotString(this)
+
+public fun String.asNodePath(): NodePath = NodePath(this)
+
+public fun String.asStringName(): StringName = StringName(this)
+
+public fun String?.asVariantString(): Variant = this?.asGodotString().use { it.asVariant() }
+
+public fun String?.asVariantStringName(): Variant = this?.asStringName().use { it.asVariant() }
+
+public fun String?.asVariantNodePath(): Variant = this?.asNodePath().use { it.asVariant() }
