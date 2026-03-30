e33f3f6992f57f900d095f7f8c303d645695ac44
Rey <89825425+kingg22@users.noreply.github.com>
2026-03-30 11:30:11 -0500

    feat(native api): add godotEnumFrom function for converting long to GodotEnum enum entry

--- kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/GodotEnum.kt
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/GodotEnum.kt
@@ -1,9 +1,14 @@
 package io.github.kingg22.godot.api
 
 import org.jetbrains.annotations.ApiStatus
+import kotlin.enums.enumEntries
 
 /** Marker interface for all generated Godot Enum. */
 @ApiStatus.NonExtendable
 public interface GodotEnum {
     public val value: Long
 }
+
+internal inline fun <reified T> godotEnumFrom(value: Long): T where T : GodotEnum, T : Enum<T> =
+    enumEntries<T>().firstOrNull { it.value == value }
+        ?: error("Enum entry with value '$value' not found for type '${T::class.simpleName}'")
