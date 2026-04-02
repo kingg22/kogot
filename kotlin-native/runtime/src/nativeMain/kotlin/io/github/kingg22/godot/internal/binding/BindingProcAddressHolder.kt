066f6502170247c5ef258c87430a9b9b6a8f52f4
Rey <89825425+kingg22@users.noreply.github.com>
2026-04-02 12:28:41 -0500

    refactor(native, runtime): simplify instance management of BindingProcAddressHolder

--- kotlin-native/runtime/src/nativeMain/kotlin/io/github/kingg22/godot/internal/binding/BindingProcAddressHolder.kt
+++ kotlin-native/runtime/src/nativeMain/kotlin/io/github/kingg22/godot/internal/binding/BindingProcAddressHolder.kt
@@ -1,5 +1,6 @@
 package io.github.kingg22.godot.internal.binding
 
+import io.github.kingg22.godot.internal.binding.BindingProcAddressHolder.Companion.instance
 import io.github.kingg22.godot.internal.ffi.GDExtensionClassLibraryPtr
 import io.github.kingg22.godot.internal.ffi.GDExtensionInterfaceGetProcAddress
 
@@ -8,16 +9,34 @@ public class BindingProcAddressHolder private constructor(
     public val library: GDExtensionClassLibraryPtr,
 ) {
     public companion object {
+        private var instance: BindingProcAddressHolder? = null
+
+        public val isInitialized: Boolean get() = instance != null
+
+        /** Shortcut to [BindingProcAddressHolder.library] of [instance] */
+        public val library: GDExtensionClassLibraryPtr get() = instance().library
+
+        /** Shortcut to [BindingProcAddressHolder.getProcAddress] of [instance] */
+        public val getProcAddress: GDExtensionInterfaceGetProcAddress get() = instance().getProcAddress
+
+        /** Retrieve the current instance or throws [IllegalStateException] if [instance] is null */
+        public fun instance(): BindingProcAddressHolder = requireNotNull(instance) {
+            "GDExtension getProcAddress holder is not initialized. Call initialize() first."
+        }
+
+        public fun instanceOrNull(): BindingProcAddressHolder? = instance
+
+        /** Initialize the holder with the given [getProcAddress] and [libraryPtr] */
         public fun initialize(
             getProcAddress: GDExtensionInterfaceGetProcAddress,
             libraryPtr: GDExtensionClassLibraryPtr,
         ) {
             instance = BindingProcAddressHolder(getProcAddress, libraryPtr)
         }
+
+        /** Reset the holder to null */
+        public fun reset() {
+            instance = null
+        }
     }
 }
-
-private var instance: BindingProcAddressHolder? = null
-
-public val bindingProcAddressHolder: BindingProcAddressHolder
-    get() = instance ?: error("GDExtension getProcAddress holder is not initialized. Call initialize() first.")
