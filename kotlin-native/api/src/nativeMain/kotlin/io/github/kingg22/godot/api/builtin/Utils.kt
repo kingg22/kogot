dfb311a49fd390d3db5144d92fc49cc332e45b33
Rey <89825425+kingg22@users.noreply.github.com>
2026-04-05 22:47:32 -0500

    feat(native, api): add experimental variant getValue operator

--- kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/builtin/Utils.kt
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/builtin/Utils.kt
@@ -1,5 +1,9 @@
 package io.github.kingg22.godot.api.builtin
 
+import io.github.kingg22.godot.api.ExperimentalGodotKotlin
+import io.github.kingg22.godot.api.builtin.internal.checkVariantCompatibility
+import kotlin.reflect.KProperty
+
 public fun String.asGodotString(): GodotString = GodotString(this)
 
 public fun String.asNodePath(): NodePath = NodePath(this)
@@ -11,3 +15,20 @@ public fun String?.asVariantString(): Variant = this?.asGodotString().use { it.a
 public fun String?.asVariantStringName(): Variant = this?.asStringName().use { it.asVariant() }
 
 public fun String?.asVariantNodePath(): Variant = this?.asNodePath().use { it.asVariant() }
+
+/**
+ * Syntactic sugar for [Variant.getValue] with type checking and inferred return type.
+ *
+ * Must prefer explicit convert methods over this operator.
+ */
+@ExperimentalGodotKotlin
+public inline operator fun <reified T> Variant.getValue(thisRef: T?, property: KProperty<*>): T {
+    val type = getType()
+    checkVariantCompatibility<T>(type)
+    if (T::class == String::class) {
+        error("Cannot provide kotlin String, must use GodotString instead or explicit convert method")
+    }
+    val value = getValue()
+    @Suppress("UNCHECKED_CAST")
+    return value as T
+}
---
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/builtin/VariantValue.kt
@@ -0,0 +1,52 @@
+package io.github.kingg22.godot.api.builtin
+
+import io.github.kingg22.godot.api.ExperimentalGodotKotlin
+
+/**
+ * Returns the value of this [Variant].
+ *
+ * Prefers explicit converter method over this function.
+ */
+@ExperimentalGodotKotlin
+public fun Variant.getValue(): Any? = when (getType()) {
+    NIL -> null
+    BOOL -> asBool()
+    INT -> asInt()
+    FLOAT -> asFloat()
+    STRING -> asString()
+    STRING_NAME -> asStringName()
+    NODE_PATH -> asNodePath()
+    VECTOR2 -> asVector2()
+    VECTOR2I -> asVector2i()
+    RECT2 -> asRect2()
+    RECT2I -> asRect2i()
+    VECTOR3 -> asVector3()
+    VECTOR3I -> asVector3i()
+    TRANSFORM2D -> asTransform2D()
+    VECTOR4 -> asVector4()
+    VECTOR4I -> asVector4i()
+    PLANE -> asPlane()
+    QUATERNION -> asQuaternion()
+    Variant.Type.AABB -> asAabb()
+    BASIS -> asBasis()
+    TRANSFORM3D -> asTransform3D()
+    PROJECTION -> asProjection()
+    COLOR -> asColor()
+    Variant.Type.RID -> asRid()
+    OBJECT -> asObject()
+    CALLABLE -> asCallable()
+    SIGNAL -> asSignal()
+    DICTIONARY -> asDictionary()
+    ARRAY -> asArray()
+    PACKED_BYTE_ARRAY -> asPackedByteArray()
+    PACKED_INT32_ARRAY -> asPackedInt32Array()
+    PACKED_INT64_ARRAY -> asPackedInt64Array()
+    PACKED_FLOAT32_ARRAY -> asPackedFloat32Array()
+    PACKED_FLOAT64_ARRAY -> asPackedFloat64Array()
+    PACKED_STRING_ARRAY -> asPackedStringArray()
+    PACKED_VECTOR2_ARRAY -> asPackedVector2Array()
+    PACKED_VECTOR3_ARRAY -> asPackedVector3Array()
+    PACKED_COLOR_ARRAY -> asPackedColorArray()
+    PACKED_VECTOR4_ARRAY -> asPackedVector4Array()
+    MAX -> error("Invalid Variant.Type.MAX")
+}
