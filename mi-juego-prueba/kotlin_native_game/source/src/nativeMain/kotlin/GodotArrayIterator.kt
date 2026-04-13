6c05a87e3fe5846e56dea4ca7b08bed94f3fcd8e
Rey <89825425+kingg22@users.noreply.github.com>
2026-04-13 16:41:26 -0500

    perf(spritebench): use List constructor to create list of GodotArray

--- mi-juego-prueba/kotlin_native_game/source/src/nativeMain/kotlin/GodotArrayIterator.kt
+++ mi-juego-prueba/kotlin_native_game/source/src/nativeMain/kotlin/GodotArrayIterator.kt
@@ -1,8 +1,4 @@
 import io.github.kingg22.godot.api.builtin.GodotArray
 import io.github.kingg22.godot.api.builtin.Variant
 
-fun <T> GodotArray<T>.asList(): List<Variant> = buildList {
-    for (i in 0 until size()) {
-        add(get(i))
-    }
-}
+fun <T> GodotArray<T>.asList(): List<Variant> = List(size().toInt()) { get(it.toLong()) }
