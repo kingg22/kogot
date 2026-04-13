013c0eb85c34a1fa6928fc911860a05d9e238ac4
Rey <89825425+kingg22@users.noreply.github.com>
2026-04-13 16:42:00 -0500

    perf(spritebench): use Double for speed calculation

--- mi-juego-prueba/kotlin_native_game/source/src/nativeMain/kotlin/Sprite.kt
+++ mi-juego-prueba/kotlin_native_game/source/src/nativeMain/kotlin/Sprite.kt
@@ -9,18 +9,17 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 @Godot class Sprite(nativePtr: COpaquePointer) : Sprite2D(nativePtr) {
     private var angle: Double = Random.nextDouble(0.0, PI * 2)
-    private var speed: Int = Random.nextInt(100..600)
+    private val speed: Double = Random.nextDouble(100.0, 600.0)
     var pos: Vector2 = Vector2.ZERO
     var windowSize: Vector2 = Vector2.ZERO
     var halfSize: Vector2 = Vector2.ZERO
 
     override fun _process(delta: Double) {
         // Prefers kotlin math functions instead of GD utils
-        pos += Vector2(x = cos(angle), y = sin(angle)) * speed.toDouble() * delta
+        pos += Vector2(x = cos(angle), y = sin(angle)) * speed * delta
         position = pos
 
         if (pos.x < halfSize.x || pos.x > windowSize.x - halfSize.x) {
