bb421cab6fa528e9d78dc3f5be9d25ede65f757c
Rey <89825425+kingg22@users.noreply.github.com>
2026-04-23 14:50:19 -0500

    feat(native, api): add checks for SignalParameterDescriptor constructor

--- kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/signal/SignalParameterDescriptor.kt
+++ kotlin-native/api/src/nativeMain/kotlin/io/github/kingg22/godot/api/signal/SignalParameterDescriptor.kt
@@ -1,10 +1,13 @@
 package io.github.kingg22.godot.api.signal
 
 import io.github.kingg22.godot.api.ExperimentalGodotKotlin
+import io.github.kingg22.godot.api.builtin.Callable
 import io.github.kingg22.godot.api.builtin.MustBeVariant
+import io.github.kingg22.godot.api.builtin.Signal
 import io.github.kingg22.godot.api.builtin.Variant
 import io.github.kingg22.godot.api.builtin.internal.variantTypeOf
 import kotlin.reflect.KType
+import kotlin.reflect.typeOf
 
 /**
  * Descriptor for a signal parameter, combining name and type information.
@@ -14,13 +17,19 @@ import kotlin.reflect.KType
  *
  * @param P The Kotlin type of this parameter
  * @property name The parameter name as it will appear in Godot
- * @property kType The Kotlin type for Variant conversion
+ * @property kType The Kotlin type of [P] for Variant conversion
  * @constructor Prefers factory method [param]
  */
-public class SignalParameterDescriptor<@MustBeVariant P> @ExperimentalGodotKotlin constructor(
+public data class SignalParameterDescriptor<@MustBeVariant P> @ExperimentalGodotKotlin constructor(
     public val name: String,
     public val kType: KType,
 ) {
+    init {
+        require(kType != typeOf<Signal>()) { "Signal cannot be a signal parameter" }
+        require(kType != typeOf<Callable>()) { "Callable cannot be a signal parameter" }
+        require(kType != typeOf<SignalParameterDescriptor<*>>()) { "SignalParameter cannot be a signal parameter" }
+    }
+
     @Suppress("DEPRECATION") // TODO find a better way to do this
     public val variantType: Variant.Type = variantTypeOf(kType)
 
