f3cac03f1d70c5215ffc0e99271357ed724a275a
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-04 13:14:53 -0500

    perf(codegen, runtime): simplify Types data class by moving deprecated and description to sealed class
    TODO move name to sealed class too, but maintain data classes

--- codegen/runtime/common/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensioninterface/Types.kt
+++ codegen/runtime/common/src/main/kotlin/io/github/kingg22/godot/codegen/models/extensioninterface/Types.kt
@@ -10,15 +10,13 @@ import kotlinx.serialization.json.JsonClassDiscriminator
 @JsonClassDiscriminator("kind")
 sealed class Types {
     abstract val name: String
-    abstract val description: List<String>
-    abstract val deprecated: Deprecated?
+    val description: List<String> = emptyList()
+    val deprecated: Deprecated? = null
 
     @Serializable
     @SerialName("enum")
     data class EnumType(
         override val name: String,
-        override val description: List<String> = emptyList(),
-        override val deprecated: Deprecated? = null,
         @SerialName("is_bitfield") val isBitfield: Boolean? = null,
         val values: List<EnumValue>,
     ) : Types() {
@@ -30,8 +28,6 @@ sealed class Types {
     @SerialName("handle")
     data class HandleType(
         override val name: String,
-        override val description: List<String> = emptyList(),
-        override val deprecated: Deprecated? = null,
         val parent: String? = null,
         @SerialName("is_const") val isConst: Boolean? = null,
         @SerialName("is_uninitialized") val isUninitialized: Boolean? = null,
@@ -39,28 +35,16 @@ sealed class Types {
 
     @Serializable
     @SerialName("alias")
-    data class AliasType(
-        override val name: String,
-        override val description: List<String> = emptyList(),
-        override val deprecated: Deprecated? = null,
-        val type: String,
-    ) : Types()
+    data class AliasType(override val name: String, val type: String) : Types()
 
     @Serializable
     @SerialName("struct")
-    data class StructType(
-        override val name: String,
-        override val description: List<String> = emptyList(),
-        override val deprecated: Deprecated? = null,
-        val members: List<Member>,
-    ) : Types()
+    data class StructType(override val name: String, val members: List<Member>) : Types()
 
     @Serializable
     @SerialName("function")
     data class FunctionType(
         override val name: String,
-        override val description: List<String> = emptyList(),
-        override val deprecated: Deprecated? = null,
         @SerialName("return_value") val returnValue: ValueType? = null,
         val arguments: List<Arguments>,
     ) : Types()
