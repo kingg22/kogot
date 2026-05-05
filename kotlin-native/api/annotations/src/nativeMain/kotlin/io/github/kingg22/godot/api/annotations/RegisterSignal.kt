442697941f940ac198c48300c357ab1ac7419a47
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-04 22:02:43 -0500

    feat(native, api, annotations): add wip design for RegisterSignal

---
+++ kotlin-native/api/annotations/src/nativeMain/kotlin/io/github/kingg22/godot/api/annotations/RegisterSignal.kt
@@ -0,0 +1,26 @@
+package io.github.kingg22.godot.api.annotations
+
+import io.github.kingg22.godot.api.builtin.Variant
+import io.github.kingg22.godot.api.global.PropertyHint
+import io.github.kingg22.godot.api.global.PropertyUsageFlags
+
+/**
+ * Requires to be annotated in a property of [io.github.kingg22.godot.api.builtin.Signal]
+ * @param name The signal name. If empty, the property name is used.
+ * @param params The signal parameters. Must be used in the same order to connect to the signal.
+ * @see io.github.kingg22.godot.api.builtin.Signal
+ */
+@Retention(SOURCE)
+@Target(PROPERTY)
+public annotation class RegisterSignal(vararg val params: Param = [], val name: String = "") {
+
+    @Retention(SOURCE)
+    @Target()
+    public annotation class Param(
+        val type: Variant.Type,
+        val name: String,
+        val hints: Array<PropertyHint> = [],
+        val hintString: String = "",
+        val usages: Array<PropertyUsageFlags> = [],
+    )
+}
