49cecb8c03ed0d99b38a94417e1c5573e08b1f2a
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-04 21:18:52 -0500

    feat(native, api, generated): add Object typealias for GodotObject
    This is PERFECT for kotlin since in Java typealias doesn't exist, so in the future when expanding to Java, doesn't have problems with java.lang.Object

---
+++ kotlin-native/api/generated/src/nativeMain/kotlin/io/github/kingg22/godot/api/core/Alias.kt
@@ -0,0 +1,5 @@
+@file:Suppress("ktlint:standard:filename") // Maintain as Alias to add more typealias in the future
+
+package io.github.kingg22.godot.api.core
+
+public typealias Object = GodotObject
