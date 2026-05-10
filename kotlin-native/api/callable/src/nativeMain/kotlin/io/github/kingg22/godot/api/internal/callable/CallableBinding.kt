ed91ec4abb7709f317a9a3f2bfd1c5b02b573f1c
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-10 15:17:54 -0500

    refactor(native, api, callable): move part of callable creation logic to CallableBinding

--- kotlin-native/api/callable/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/callable/CallableBinding.kt
+++ kotlin-native/api/callable/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/callable/CallableBinding.kt
@@ -1,21 +1,120 @@
 package io.github.kingg22.godot.api.internal.callable
 
+import io.github.kingg22.godot.api.GodotNative
 import io.github.kingg22.godot.internal.binding.BindingProcAddressHolder
 import io.github.kingg22.godot.internal.binding.CallableBinding
 import io.github.kingg22.godot.internal.binding.InternalBinding
+import io.github.kingg22.godot.internal.binding.VariantBinding
+import io.github.kingg22.godot.internal.binding.getInstance
+import io.github.kingg22.godot.internal.ffi.GDExtensionCallError
+import io.github.kingg22.godot.internal.ffi.GDExtensionCallErrorType
 import io.github.kingg22.godot.internal.ffi.GDExtensionCallableCustomCall
 import io.github.kingg22.godot.internal.ffi.GDExtensionCallableCustomInfo2
-import kotlinx.cinterop.COpaquePointer
-import kotlinx.cinterop.CValue
-import kotlinx.cinterop.StableRef
-import kotlinx.cinterop.cValue
-import kotlinx.cinterop.memScoped
+import io.github.kingg22.godot.internal.ffi.GDExtensionConstVariantPtr
+import io.github.kingg22.godot.internal.ffi.GDExtensionConstVariantPtrVar
+import io.github.kingg22.godot.internal.ffi.GDExtensionInt
+import io.github.kingg22.godot.internal.ffi.GDExtensionVariantPtr
+import kotlinx.cinterop.*
 
+/**
+ * This is a low-level binding for custom callables. You must use `CallabeFactory` to create custom callables or use the `Callable` class.
+ *
+ * The initialization of `CallableFactory` link properties of this class. Must not be invoked before that happens,
+ * otherwise the binding throws errors with null values.
+ *
+ * Currently missing correct support to convert arguments to kotlin types. So must use `Any?` or exact `Variant` types,
+ * `Variant` itself as argument is not supported.
+ *
+ * Currently, all callables are `static` for Godot, missing linking to the object. This can lead to incorrect freeing.
+ */
 @InternalBinding
 public object CallableBinding {
+    public var onError: (message: String) -> Unit = ::println
+    public var onVariantToKotlin: (GDExtensionConstVariantPtr?) -> Any? = { null }
+    public var onToVariant: (Any?) -> GodotNative? = { null }
+
+    // Create the GDExtensionCallableCustomInfo2 struct using cValue
+    public fun createCustomInfo2(lambda: Function<*>, objectId: ULong = 0uL): CValue<GDExtensionCallableCustomInfo2> =
+        createCustomInfo2(
+            // Create a KotlinCallable wrapper and store it in a StableRef on userdata
+            storeKotlinFunctionAsKotlinCallable(lambda),
+            createCustomCall(),
+            objectId,
+        )
+
+    public fun createCustomCall(): GDExtensionCallableCustomCall = staticCFunction {
+            userdata: COpaquePointer?,
+            arguments: CArrayPointer<GDExtensionConstVariantPtrVar>?,
+            argumentCount: GDExtensionInt,
+            rReturn: GDExtensionVariantPtr?,
+            rError: CPointer<GDExtensionCallError>?,
+        ->
+        val callable = userdata?.getInstance<KotlinCallable>()
+
+        if (callable == null || rReturn == null || rError == null) {
+            onError(
+                "Invalid arguments received from Godot to Callable custom call: $arguments, $argumentCount, $rReturn, $rError, $userdata == $callable",
+            )
+            return@staticCFunction
+        }
+
+        fun fillCallError(
+            error: GDExtensionCallErrorType,
+            expected: Int = callable.arity().toInt(),
+            argument: Int = argumentCount.toInt(),
+        ) {
+            val callError = rError.pointed
+            callError.error = error
+            callError.expected = expected
+            callError.argument = argument
+        }
+
+        if (argumentCount != callable.arity()) {
+            onError("[kogot]: Error: callable called with $argumentCount arguments, expected ${callable.arity()}")
+            fillCallError(
+                if (argumentCount < callable.arity()) {
+                    GDEXTENSION_CALL_ERROR_TOO_FEW_ARGUMENTS
+                } else {
+                    GDEXTENSION_CALL_ERROR_TOO_MANY_ARGUMENTS
+                },
+            )
+            return@staticCFunction
+        }
+
+        if (argumentCount != 0L && arguments == null) {
+            onError("[kogot]: Error: callable called with null array of arguments, expected $argumentCount")
+            fillCallError(GDEXTENSION_CALL_ERROR_INVALID_ARGUMENT)
+            return@staticCFunction
+        }
+
+        // FIXME validate arguments order is preserved
+        val arguments = Array(argumentCount.toInt()) { i ->
+            // FIXME requires apropiate conversion to Kotlin types. Requires supports downcast, Variant, KString
+            onVariantToKotlin(arguments?.get(i))
+        }
+
+        val result: Any? = runCatching { callable.invoke(arguments) }.getOrElse { exception ->
+            onError(
+                "[kogot]: Error calling callable ${callable.arity()} args, fallback to null: $exception",
+            )
+            fillCallError(GDEXTENSION_CALL_ERROR_INVALID_METHOD)
+            return@staticCFunction
+        }
+
+        // TODO: properly convert return types to Kotlin types
+        // (aka if the user request T, the argument must preserve the type T, not unbox or box unrequested)
+        val resultVariant = onToVariant(result) ?: run {
+            onError("[kogot]: Error converting callable result ($result) to Variant, fallback to null")
+            fillCallError(GDEXTENSION_CALL_ERROR_INVALID_METHOD)
+            return@staticCFunction
+        }
+
+        fillCallError(GDEXTENSION_CALL_OK, 0, 0)
+        VariantBinding.instance.newCopyRaw(rReturn, resultVariant.rawPtr)
+    }
 
     // Call the low-level Godot function to create custom callable and fill the callable object
-    public fun createCustom(callableCustomInfo2: CValue<GDExtensionCallableCustomInfo2>, callablePtr: COpaquePointer) {
+    public fun createCustom2(callableCustomInfo2: CValue<GDExtensionCallableCustomInfo2>, callablePtr: COpaquePointer) {
         memScoped {
             CallableBinding.instance.customCreate2Raw(
                 callablePtr,
@@ -24,7 +123,8 @@ public object CallableBinding {
         }
     }
 
-    public fun storeKotlinCallable(lambda: Function<*>): COpaquePointer =
+    /** Wraps a [Kotlin function][Function] into a [KotlinCallable] using [StableRef] */
+    public fun storeKotlinFunctionAsKotlinCallable(lambda: Function<*>): COpaquePointer =
         StableRef.create(wrapLambda(lambda)).asCPointer()
 
     /**
@@ -34,7 +134,7 @@ public object CallableBinding {
      * @param call The custom function to call this callable
      * @param objectId 0 means _custom_, not bind to object
      */
-    public fun createCallableCustomInfo2(
+    public fun createCustomInfo2(
         userdata: COpaquePointer,
         call: GDExtensionCallableCustomCall,
         objectId: ULong = 0uL,
--- kotlin-native/api/generated/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/callable/CallableFactory.kt
+++ kotlin-native/api/generated/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/callable/CallableFactory.kt
@@ -5,23 +5,10 @@ import io.github.kingg22.godot.api.builtin.Variant
 import io.github.kingg22.godot.api.builtin.internal.anyToVariantOrNull
 import io.github.kingg22.godot.api.builtin.internal.getValue
 import io.github.kingg22.godot.api.internal.UsedFromCodegenGeneratedCode
-import io.github.kingg22.godot.api.internal.callable.CallableBinding.createCallableCustomInfo2
-import io.github.kingg22.godot.api.internal.callable.CallableBinding.createCustom
-import io.github.kingg22.godot.api.internal.callable.CallableBinding.storeKotlinCallable
 import io.github.kingg22.godot.api.utils.GD
 import io.github.kingg22.godot.api.utils.pushError
 import io.github.kingg22.godot.internal.binding.InternalBinding
-import io.github.kingg22.godot.internal.binding.VariantBinding
-import io.github.kingg22.godot.internal.binding.getInstance
-import io.github.kingg22.godot.internal.ffi.GDExtensionCallErrorType
-import io.github.kingg22.godot.internal.ffi.GDExtensionCallableCustomInfo2
 import io.github.kingg22.godot.internal.ffi.GDExtensionConstVariantPtr
-import io.github.kingg22.godot.internal.ffi.GDExtensionConstVariantPtrVar
-import kotlinx.cinterop.CArrayPointer
-import kotlinx.cinterop.CValue
-import kotlinx.cinterop.get
-import kotlinx.cinterop.pointed
-import kotlinx.cinterop.staticCFunction
 
 /**
  * Factory for creating Godot custom Callables from Kotlin lambdas.
@@ -35,96 +22,22 @@ import kotlinx.cinterop.staticCFunction
 @UsedFromCodegenGeneratedCode
 @InternalBinding
 public object CallableFactory {
+    init {
+        CallableBinding.onError = GD::pushError
+        CallableBinding.onVariantToKotlin = ::reinterpretedVariantToKotlin
+        CallableBinding.onToVariant = ::anyToVariantOrNull
+    }
+
     /** See [Callable] class and factory function */
     @UsedFromCodegenGeneratedCode
     public fun create(lambda: Function<*>, callable: Callable) {
         check(callable.isNull()) { "Callable must be null to create a custom callable" }
-        val callableCustomInfo2 = createImpl(lambda)
-        createCustom(callableCustomInfo2, callable.rawPtr)
+        val callableCustomInfo2 = CallableBinding.createCustomInfo2(lambda)
+        CallableBinding.createCustom2(callableCustomInfo2, callable.rawPtr)
     }
 
     /** See [Callable] class and factory function. Create the empty Callable */
     public fun create(lambda: Function<*>): Callable = Callable(null).apply { create(lambda, this) }
-
-    // Create the GDExtensionCallableCustomInfo2 struct using cValue
-    public fun createImpl(lambda: Function<*>): CValue<GDExtensionCallableCustomInfo2> = createCallableCustomInfo2(
-        // Create a KotlinCallable wrapper and store it in a StableRef on userdata
-        storeKotlinCallable(lambda),
-        staticCFunction {
-                userdata,
-                arguments: CArrayPointer<GDExtensionConstVariantPtrVar>?,
-                argumentCount,
-                rReturn,
-                rError,
-            ->
-            val callable = userdata?.getInstance<KotlinCallable>()
-
-            if (callable == null || rReturn == null || rError == null) {
-                GD.pushError(
-                    "Invalid arguments received from Godot to Callable custom call: $arguments, $argumentCount, $rReturn, $rError, $userdata == $callable",
-                )
-                return@staticCFunction
-            }
-
-            fun fillCallError(
-                error: GDExtensionCallErrorType,
-                expected: Int = callable.arity().toInt(),
-                argument: Int = argumentCount.toInt(),
-            ) {
-                val callError = rError.pointed
-                callError.error = error
-                callError.expected = expected
-                callError.argument = argument
-            }
-
-            if (argumentCount != callable.arity()) {
-                GD.pushError(
-                    "[kogot]: Error: callable called with $argumentCount arguments, expected ${callable.arity()}",
-                )
-                fillCallError(
-                    if (argumentCount < callable.arity()) {
-                        GDEXTENSION_CALL_ERROR_TOO_FEW_ARGUMENTS
-                    } else {
-                        GDEXTENSION_CALL_ERROR_TOO_MANY_ARGUMENTS
-                    },
-                )
-                return@staticCFunction
-            }
-
-            if (argumentCount != 0L && arguments == null) {
-                GD.pushError(
-                    "[kogot]: Error: callable called with null array of arguments, expected $argumentCount",
-                )
-                fillCallError(GDEXTENSION_CALL_ERROR_INVALID_ARGUMENT)
-                return@staticCFunction
-            }
-
-            // FIXME validate arguments order is preserved
-            val arguments = Array(argumentCount.toInt()) { i ->
-                // FIXME requires apropiate conversion to Kotlin types. Requires supports downcast, Variant, KString
-                reinterpretedVariantToKotlin(arguments?.get(i))
-            }
-
-            val result: Any? = runCatching { callable.invoke(arguments) }.getOrElse { exception ->
-                GD.pushError(
-                    "[kogot]: Error calling callable ${callable.arity()} args, fallback to null: $exception",
-                )
-                fillCallError(GDEXTENSION_CALL_ERROR_INVALID_METHOD)
-                return@staticCFunction
-            }
-
-            // TODO: properly convert return types to Kotlin types
-            // (aka if the user request T, the argument must preserve the type T, not unbox or box unrequested)
-            val resultVariant = anyToVariantOrNull(result) ?: run {
-                GD.pushError("[kogot]: Error converting callable result ($result) to Variant, fallback to null")
-                fillCallError(GDEXTENSION_CALL_ERROR_INVALID_METHOD)
-                return@staticCFunction
-            }
-
-            fillCallError(GDEXTENSION_CALL_OK, 0, 0)
-            VariantBinding.instance.newCopyRaw(rReturn, resultVariant.rawPtr)
-        },
-    )
 
     public fun reinterpretedVariantToKotlin(argument: GDExtensionConstVariantPtr?): Any? =
         if (argument == null) null else Variant(argument).getValue()
