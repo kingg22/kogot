fbb1f843e811ac313c08bd794623bcb04f540864
Rey <89825425+kingg22@users.noreply.github.com>
2026-05-12 16:28:26 -0500

    feat(native, api, generated): add checkGodotError overload with error Long value

--- kotlin-native/api/generated/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/Utils.kt
+++ kotlin-native/api/generated/src/nativeMain/kotlin/io/github/kingg22/godot/api/internal/Utils.kt
@@ -9,3 +9,12 @@ public inline fun checkGodotError(context: String, error: GodotError) {
         "Godot Error: $error (${error.value}) in $context"
     }
 }
+
+@UsedFromCodegenGeneratedCode
+@Suppress("NOTHING_TO_INLINE")
+public inline fun checkGodotError(context: String, error: Long) {
+    check(error == GodotError.OK.value) {
+        val errorEntry = GodotError.entries.firstOrNull { it.value == error }
+        "Godot Error: ${errorEntry ?: "'unknown enum entry'"} ($error) in $context"
+    }
+}
